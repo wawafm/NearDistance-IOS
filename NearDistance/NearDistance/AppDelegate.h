ab1e1495c00ec4b19d15ae43c1b546278cd62559
lishchao <18710861151@163.com>
2015-09-28 18:06:55 +0800

    邻距项目第一次提交

---
+++ NearDistance/NearDistance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NearDistance
+//
+//  Created by 李世超 on 15/9/28.
+//  Copyright © 2015年 李世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NearDistance/NearDistance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearDistance
+//
+//  Created by 李世超 on 15/9/28.
+//  Copyright © 2015年 李世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
